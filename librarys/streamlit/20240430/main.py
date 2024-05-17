1a5879754e2dbe5f7af49962a910123579e3ae59
devseunggwan <yanhis97531@gmail.com>
2024-05-17 17:50:13 +0900

    💥 NFTCuration - 프롬프트 세부 수정

--- librarys/streamlit/20240430/main.py
+++ librarys/streamlit/20240430/main.py
@@ -4,6 +4,7 @@ import random
 import time
 
 import httpx
+import openai
 import pandas as pd
 import streamlit as st
 from dotenv import load_dotenv
@@ -40,67 +41,68 @@ class NFTCurationBot:
         )
 
         self.prompt_description = """
-역할
+1. 역할
 - 당신은 NFT 컬렉션 전문가며 큐레이터를 하면서 사람들에게 NFT에 대해 내용을 전달하는 역할입니다.
 - 당신은 주어진 NFT 이미지를 기반으로 컬렉션에 대한 묘사를 작성해야 합니다.
 
-지시사항
-1. 작성 항목 및 설명
-    - 진부하지 않고 생동감있게 표현해주세요.
-    - 이미지의 주요 특징을 강조해주세요. 
-    - 첫번째, 두번째 이미지는 어떻다. 라고 설명하지말고 말아주세요.
-    - 이미지의 개별적인 묘사보다 종합적인 묘사를 작성해주세요.
-2. 문장 작성 방법
-    - 평가와 묘사는 최대 500자로 제한합니다.
-    - 만약 500자에서 문장이 마무리되지 않는다면, 300자 이상 작성해도 좋으니 문장을 마무리해주세요.
-    - 평가와 묘사 등을 문단이나 구분해서 작성하지 말아주세요.
-    - 컬랙션 이름을 작성할 떄는 원문 그대로 작성해주세요.
-    - 답변은 한국어로 작성해주세요.
-    - 문법, 맞춤법, 띄어쓰기를 꼭 지켜주세요.
+2. 작성 항목 및 설명
+- 진부하지 않고 생동감있게 표현해주세요.
+- 이미지의 주요 특징을 강조해주세요. 
+- 첫번째, 두번째 이미지는 어떻다. 라고 설명하지말고 말아주세요.
+- 이미지의 개별적인 묘사보다 종합적인 묘사를 작성해주세요.
+    
+3. 문장 작성 방법
+- 답변은 한국어로 작성해주세요.
+- 평가와 묘사는 최대 200자로 제한합니다.
+- 평가와 묘사 등을 문단이나 구분해서 작성하지 말아주세요.
+- 컬랙션 이름을 작성할 떄는 원문 그대로 작성해주세요.
+- 문법, 맞춤법, 띄어쓰기를 꼭 지켜주세요.
 
-컬렉션 추가 정보
+4. 추가 정보
 - 컬렉션 이름: {collection_name}
 - 컬렉션 설명: {collection_description}
 """
         self.prompt_tag = """
-역할
+1. 역할
 - 당신은 NFT 컬렉션 전문가며 큐레이터를 하면서 사람들에게 NFT에 대해 내용을 전달하는 역할입니다.
 - 당신은 주어진 NFT 이미지, 이름, 설명과 이를 바탕으로 요약된 내용을 기반으로 컬렉션에 대한 태그를 작성해야 합니다.
 
-지시사항
-1. 작성 항목 및 설명
-    - 이미지를 종합적으로 확인하고 컬랙션을 대표하는 키워드를 작성해주세요.
-    - NFT의 이름, NFT 이더리움, 블록체인 네트워크 같은 NFT에서 일반적으로 이야기하는 키워드를 넣지도 말고 포함하지도 말아주세요.
-    - 개성, 다양성, 독창성, 창의성 같이 모호하고, 일반적, 진부적으로 이미지를 표현하는 키워드는 넣지 말아주세요.
-    - 이미지 하나에서만 확인할 수 있는 키워드는 제외하고 작성해주세요.
-    - NFT 컬랙션 이미지에 대해서 개별적인 묘사보다는 종합적인 묘사로 어올리는 키워드를 작성해주세요.
-2. 문장 작성 방법
-    - 태그는 List(str) 형태로 쉼표(,)로 구분해주시고 예시를 보고 꼭 지켜주세요. (예: ["태그1", "태그2", "태그3"])
-    - 태그는 8~10개 작성해주세요.
-    - 태그에 스페이스를 넣지 마세요.
-    - 태그는 한국어로만 작성해주세요.
+2. 작성 항목 및 설명
+- 이미지를 종합적으로 확인하고 컬랙션을 대표하는 키워드를 작성해주세요.
+- NFT의 이름, NFT 이더리움, 블록체인 네트워크 같은 NFT에서 일반적으로 이야기하는 키워드를 넣지도 말고 포함하지도 말아주세요.
+- 개성, 다양성, 독창성, 창의성 같이 모호하고, 일반적, 진부적으로 이미지를 표현하는 키워드는 넣지 말아주세요.
+- 이미지 하나에서만 확인할 수 있는 키워드는 제외하고 작성해주세요.
+- NFT 컬랙션 이미지에 대해서 개별적인 묘사보다는 종합적인 묘사로 어올리는 키워드를 작성해주세요.
 
-컬렉션 추가 정보
+3. 문장 작성 방법
+- 태그는 List(str) 형태로 쉼표(,)로 구분해주시고 예시를 보고 꼭 지켜주세요. (예: ["태그1", "태그2", "태그3"])
+- 태그는 8~10개 작성해주세요.
+- 태그에 스페이스를 넣지 마세요.
+- 태그는 한국어로만 작성해주세요.
+
+4. 추가 정보
 - 컬렉션 이름: {collection_name}
 - 컬렉션 설명: {collection_description}
 - 컬랙션 요약: {ai_curation}
-
-        """
+"""
         self.prompt_curation = """
-역할
+1. 역할
 - 당신은 NFT 컬렉션 전문가며 큐레이터를 하면서 사람들에게 NFT에 대해 내용을 전달하는 역할입니다.
 - 당신은 NFT 컬랙션에 대한 요약과 태그를 보고 종합적으로 한 줄 요약이 필요합니다.
-- 아이들에게 NFT 컬렉션에 대해 설명하는 것처럼 친근하게 쉽고 명확하게 작성해주세요. 
-- 말투를 부드럽게 해주세요. `다.` 로 끝나지 말고 `요.` 로 끝내주세요.
-- 90~120자 내외로 한국어로 작성해주세요.
+- 45자 이상 55자 사이의 한국어 문장으로 작성해주세요.
+- 마지막에 NFT 컬랙션 이라는 워딩을 사용하지 마세요.
 
-출력 결과 예시
-- Azuki 컬렉션은 현대적인 감각과 전통적인 요소를 접목해 다양한 캐릭터와 깊이 있는 이야기를 통해 웹3 세계에서 소속감과 성장 경험을 제공하는 디지털 아트 작품이에요.
-- Azuki 컬렉션은 애니메이션과 일본 문화가 결합된 멋진 캐릭터들을 통해 독특한 디지털 세계를 소개하며, 웹3 커뮤니티와 함께 성장하는 경험을 제공해요.
+2. 출력 결과 예시
+- 일본의 역사적 상징인 사무라이를 NFT 애니메이션으로 만나다
+- 스트릿 스타일 네오치비 PFP NFT, 밝고 생기 있는 컬러 팔레트가 특징
+- 2017년 시작된 픽셀 아트, 독창적 캐릭터로 디지털 예술을 혁신
+- 도시의 개성 넘치는 'Personas'와 Timefall Valley의 다채로운 스토리
+- 미래지향적 큐브 NFT, 첨단 기술과 빛의 교차가 돋보임
+- 20세기 마스터들의 영감을 현대적 콜라주로 재해석
 
-NFT 컬랙션에 대한 요약
+3. NFT 컬랙션에 대한 요약
 {ai_curation}
-        """
+"""
 
         self.logger = logging.getLogger(st.__name__)
 
@@ -320,33 +322,37 @@ NFT 컬랙션에 대한 요약
 
                 for i in range(self.question_count):
                     start_time = time.time()
-                    with st.status(
-                        f"⏳ Generating NFT Curation ({i + 1})", expanded=True
-                    ):
-                        nft_description = self.get_nft_description(
-                            nft_images, collection_name, collection_description
-                        )
-                        nft_tag = self.get_nft_tag(
-                            nft_images,
-                            collection_name,
-                            collection_description,
-                            nft_description,
-                        )
-                        nft_curation = self.get_nft_curation(nft_description)
 
-                        st.markdown("##### NFT 설명")
-                        st.write(nft_description)
-                        st.markdown("##### NFT 태그")
-                        st.write(nft_tag)
-                        st.markdown("##### NFT 한줄 요약")
-                        st.write(nft_curation)
+                    try:
+                        with st.status(
+                            f"⏳ Generating NFT Curation ({i + 1})", expanded=True
+                        ):
+                            nft_description = self.get_nft_description(
+                                nft_images, collection_name, collection_description
+                            )
+                            nft_tag = self.get_nft_tag(
+                                nft_images,
+                                collection_name,
+                                collection_description,
+                                nft_description,
+                            )
+                            nft_curation = self.get_nft_curation(nft_description)
 
-                        elapsed_time = time.time() - start_time
-                        st.write(f"⏱️ Elapsed Time: {elapsed_time:.2f} sec")
+                            st.markdown("##### 큐레이션 한마디")
+                            st.write(nft_curation)
+                            st.markdown("##### NFT 설명")
+                            st.write(nft_description)
+                            st.markdown("##### NFT 태그")
+                            st.write(nft_tag)
 
-                        self.logger.info(
-                            f"Network: {network}, Collection ID: {collection_id}, Elapsed Time: {elapsed_time:.2f} sec"
-                        )
+                            elapsed_time = time.time() - start_time
+                            st.write(f"⏱️ Elapsed Time: {elapsed_time:.2f} sec")
+
+                            self.logger.info(
+                                f"Network: {network}, Collection ID: {collection_id}, Elapsed Time: {elapsed_time:.2f} sec"
+                            )
+                    except openai.BadRequestError as E:
+                        st.write(E)
 
                 st.subheader("NFT Description")
                 st.write(collection_description)

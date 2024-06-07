48d04496ab8b9315ceda2921e386f18fb1eeadb9
Seunggwan Song <yanhis97531@gmail.com>
2024-06-07 04:42:31 +0000

    🏃add dependency, output

--- IaaS/terraform/playground/hello-world/local.tf
+++ IaaS/terraform/playground/hello-world/local.tf
@@ -1,11 +1,16 @@
 resource "local_file" "hello" {
     filename = var.filename
-    content = var.content
+    content = "${random_string.random_code.id}"
     file_permission = "0700"
+    depends_on = [ random_string.random_code ]
 }
 
 resource "random_string" "random_code" {
     length = 5
     special = false
     upper = false
+}
+
+output "random_code" {
+    value = random_string.random_code.id
 }

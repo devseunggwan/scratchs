bb2146d4bd6453b4e150556d8f5653e592a40b64
Seunggwan Song <yanhis97531@gmail.com>
2025-01-14 02:39:05 +0000

    ⚡JS7 JobScheduler Agent Container 다중화
    details
    * 컨테이너 다중화를 통해서 분산처리 할 수 있도록 구성하였습니다.
    * 다만, Agent 다중화 이후 JOC Cockpit에서 Agent를 연결해주어야 합니다.

--- platforms/js7/docker-compose/init.sh
+++ platforms/js7/docker-compose/init.sh
@@ -1,10 +1,15 @@
-mkdir js7-agent-primary
+mkdir js7-agent-primary-1
+mkdir js7-agent-primary-2
+mkdir js7-agent-primary-3
 mkdir js7-controller-primary
 mkdir js7-joc-primary-config
 mkdir js7-joc-primary-logs
 mkdir db_data
 
 docker compose --env-file ./.env.sample -f docker-compose.yaml up -d
+
+echo "Waiting for the database to start..."
+sleep 20
 
 cp -f hibernate.cfg.xml js7-joc-primary-config/
 docker-compose exec js7-joc-primary /bin/bash -c /opt/sos-berlin.com/js7/joc/install/joc_install_tables.sh
